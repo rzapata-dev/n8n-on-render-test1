1374c9e0d3aa169f22c80e26031baff863dea1a5
RZapata <zapatarichard08@gmail.com>
2025-08-07 20:31:57 -0500

    agregar script de prueva IPV6

--- test-ipv6.sh
+++ test-ipv6.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "🔍 Probando conectividad IPv6..."
+echo " ~M Probando conectividad IPv6..."
 ping6 -c 3 db.lcjdpyxcxffgwtxqmchh.supabase.co || echo "❌ No hay conexión IPv6"
 echo "✅ Script terminado"
-
